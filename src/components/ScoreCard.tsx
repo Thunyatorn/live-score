a84d4e1c50c4e573c7f4e0944316591cab549d8a
Blackslex <tjnjtjcj@gmail.com>
2023-07-30 01:07:41 +0700

    add

--- src/components/ScoreCard.tsx
+++ src/components/ScoreCard.tsx
@@ -6,8 +6,10 @@ interface IScoreCard {
 export const ScoreCard = ({ name, score }: IScoreCard) => {
   return (
     <div className="flex flex-col rounded-lg p-3 shadow-md">
-      <h2 className="text-3xl font-semibold">{name}</h2>
-      <p className="text-2xl">คะแนนรวม : {score}</p>
+      <center>
+        <h2 className="text-3xl font-semibold">{name}</h2>
+        <p className="text-2xl font-semibold">{score}</p>
+      </center>
     </div>
   );
 };
