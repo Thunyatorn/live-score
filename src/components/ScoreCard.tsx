ec09189943eeb7fb63a8e65f03b36a6086001f71
Blackslex <tjnjtjcj@gmail.com>
2023-09-30 19:25:13 +0700

    feat: mobile

--- src/components/ScoreCard.tsx
+++ src/components/ScoreCard.tsx
@@ -8,20 +8,46 @@ interface IScoreCard {
 
 export const ScoreCard = ({ name, score, is_skeleton }: IScoreCard) => {
   return (
-    <div className="w-1/4 flex flex-col rounded-lg bg-purple-100 dark:bg-purple-200 border border-purple-700 text-purple-950 p-4 shadow-md">
+    <div className="hidden w-1/4 flex-col rounded-lg border border-purple-700 bg-purple-100 p-4 text-purple-950 shadow-md dark:bg-purple-200 sm:flex">
       {!is_skeleton ? (
         <h2 className="text-center text-xl font-semibold">{name}</h2>
       ) : (
         <div className="flex justify-center">
-          <div className="mt-3 h-8 w-full animate-pulse rounded-md bg-gray-50" />
+          <div className="mt-3 h-8 w-full animate-pulse rounded-md bg-gray-50 dark:bg-gray-900" />
         </div>
       )}
       <ScoreGauge score={!is_skeleton ? score : 0} />
       {!is_skeleton ? (
-        <p className="mt-3 text-center text-xl text-purple-700">{score} point(s)</p>
+        <p className="mt-3 text-center text-xl text-purple-700">
+          {score} point(s)
+        </p>
       ) : (
         <div className="flex justify-center">
-          <div className="mt-3 h-6 w-1/4 animate-pulse rounded-md bg-gray-50" />
+          <div className="mt-3 h-6 w-1/4 animate-pulse rounded-md bg-gray-50 dark:bg-gray-900" />
+        </div>
+      )}
+    </div>
+  );
+};
+
+export const ScoreCardMobile = ({ name, score, is_skeleton }: IScoreCard) => {
+  return (
+    <div className="flex w-2/3 flex-col rounded-lg border border-purple-700 bg-purple-100 p-4 text-purple-950 shadow-md dark:bg-purple-200 sm:hidden">
+      {!is_skeleton ? (
+        <h2 className="text-center text-sm font-semibold">{name}</h2>
+      ) : (
+        <div className="flex justify-center">
+          <div className="mt-3 h-8 w-full animate-pulse rounded-md bg-gray-50 dark:bg-gray-900" />
+        </div>
+      )}
+      <ScoreGauge score={!is_skeleton ? score : 0} />
+      {!is_skeleton ? (
+        <p className="mt-3 text-center text-sm text-purple-700">
+          {score} point(s)
+        </p>
+      ) : (
+        <div className="flex justify-center">
+          <div className="mt-3 h-6 w-1/4 animate-pulse rounded-md bg-gray-50 dark:bg-gray-900" />
         </div>
       )}
     </div>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,7 +10,14 @@ export default function Home() {
         <link rel="icon" href="/tugmos.ico" />
       </Head>
       <div className="flex w-full flex-col items-center py-6">
-        <h1 className="text-5xl font-bold dark:text-slate-300">TUGMOs 20th Live Scoreboard</h1>
+        {/* desktop */}
+        <h1 className="hidden text-center text-5xl font-bold dark:text-slate-300 sm:flex">
+          TUGMOs 20th Live Scoreboard
+        </h1>
+        {/* mobile */}
+        <h1 className="flex text-center text-3xl font-bold dark:text-slate-300 sm:hidden">
+          TUGMOs 20th Live Scoreboard
+        </h1>
         <ScoreTable />
       </div>
     </>
